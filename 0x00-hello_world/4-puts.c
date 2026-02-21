765f27d9c1f0781587becdd82e3d622ed64c5ff6
osoticharles <osoticharles@gmail.com>
2026-02-21 11:02:53 +0000

    Completed project - Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+	return (0);
+}
